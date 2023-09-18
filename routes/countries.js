20070152780c902871f398c85f922a977b1444c3
Rob Elliott <elliott@iu.edu>
2023-09-18 14:18:53 -0400

    End of v3

--- routes/countries.js
+++ routes/countries.js
@@ -6,7 +6,23 @@ const Country = require('models/Country');
 router.get('/', async (req, res) => {
     const countries = await Country.findAll();
    //res.send(countries);
-   res.render('countries', {title: 'Countries', countrylist: countries});
+   res.render('countries/index', {title: 'Countries', countrylist: countries});
+});
+
+/* GET a single country from the database */
+router.get('/:id', async (req, res) => {
+    const requestedId = req.params.id;
+    
+    // Query the database to get the country with that ID
+    const country = await Country.findOne({
+        where: {id: requestedId}
+    });
+    
+    // Render the detail view for that country
+    res.render('countries/detail', {
+        title: 'Country Details: ' + country.name,
+        countryItem: country
+    });
 });
 
 module.exports = router;
